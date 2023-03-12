65f598525ee3b5a54730bcd036af083fbd4707ea
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-03-12 13:26:53 +0330

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace project
+{
+    class test
+    {
+        public static void Main()
+        {
+            Double x = Convert.ToInt32(Console.ReadLine());
+            Double y = Convert.ToInt32(Console.ReadLine());
+            Double z=Math.Sqrt(Math.Cos(x)*Math.Abs(y-x)+Math.Sqrt(Math.Asin(x)));
+            Console.WriteLine(z);
+        }
+    }
+}
